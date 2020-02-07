4093edec9a4baa976537c44a541005cc83fa8489
Hector Rodriguez Fornies <torhector2@gmail.com>
2020-02-07 09:40:34 +0700

    chore: add redux and react-redux to the project

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(reducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! Redux!!!!</Text>
+      </View>
+    </Provider>
   );
 }
 
---
+++ actions/index.js
@@ -0,0 +1,9 @@
+export const ADD_DECK = 'ADD_DECK'
+
+
+export function addDeck (deck) {
+    return {
+      type: ADD_DECK,
+      deck,
+    }
+  }
