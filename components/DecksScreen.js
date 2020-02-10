c03eed08570e775c31e7467791e13d01b5c3ac93
Hector Rodriguez Fornies <torhector2@gmail.com>
2020-02-10 10:41:43 +0700

    style: add icons to the tab bar

--- components/DecksScreen.js
+++ components/DecksScreen.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import DecksListScreen from './DecksListScreen'
 import NewDeckScreen from './NewDeckScreen'
+import { AntDesign } from '@expo/vector-icons';
 
 const Tab = Platform.OS === 'ios' ? 
   createBottomTabNavigator() :
@@ -11,7 +12,20 @@ const Tab = Platform.OS === 'ios' ?
 
 export default function DecksScreen({ navigation }) {
     return (
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Decks') {
+            iconName = 'book';
+          } else if (route.name === 'Add Deck') {
+            iconName = 'addfolder';
+          }
+
+          // You can return any component that you like here!
+          return <AntDesign name={iconName} size={size} color={color} />;
+        },
+      })}>
           <Tab.Screen name="Decks" component={DecksListScreen} />
           <Tab.Screen name="Add Deck" component={NewDeckScreen} />
       </Tab.Navigator>
